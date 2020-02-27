191bbe14839ee7b2cefaac332cd31e3cdec61222
amzn1963 <mjcranes@gmail.com>
2020-02-27 23:20:40 +0000

    initial commit

---
+++ celtic_artifacts/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for celtic_artifacts project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celtic_artifacts.settings')
+
+application = get_asgi_application()
